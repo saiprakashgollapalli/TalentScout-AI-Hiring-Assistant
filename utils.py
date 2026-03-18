cb46e7208f3e77195d56748573cb9e5f4f76a494
Sai Prakash Gollapalli <saiprakashgollapalli@users.noreply.github.com>
2026-03-18 21:24:37 +0530

    Final UI stabilization fix and All changes done

--- utils.py
+++ utils.py
@@ -6,11 +6,15 @@ def validate_email(email):
 def validate_phone(phone):
     return phone.isdigit() and len(phone) == 10
 
+
+
 def detect_sentiment(text):
     negative_words = ["bad", "hard", "difficult", "confusing"]
 
+    words = re.findall(r'\b\w+\b', text.lower())
+
     for word in negative_words:
-        if word in text.lower():
+        if word in words:
             return "negative"
 
     return "neutral"
